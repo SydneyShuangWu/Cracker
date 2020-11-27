edfc31c6ee0d0bc10e03aff067745246340ed1b7
Sydney Wu <b05703070@ntu.edu.tw>
2020-11-27 11:57:59 +0800

    Install SwiftLint

---
+++ Cracker/Cracker/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Cracker
+//
+//  Created by Jovan ho on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
